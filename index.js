db1b14d2d029e3a1726c810e091b9620a3d2dcbf
Palakgupta2002 <pg948303@gmail.com>
2023-03-17 13:59:35 +0530

    sun

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("mouthfun").addEventListener("click",(()=>{
+  var mouthtext= document.createElement("div");
+  mouthtext.innerText="Hello i am sun";
+  document.getElementById("sunmouthtext").append(mouthtext);
+
+}))
